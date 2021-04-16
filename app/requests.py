4671cb801f21d138fc4493869464a56f2b3773c1
Caleb Tony <calebmbugua@gmail.com>
2021-04-16 13:43:14 +0300

    added the process_results function to parse list into list of movie objs

--- app/requests.py
+++ app/requests.py
@@ -8,6 +8,22 @@ MovieObj = movie.Movie
 api_key = app.config['API_KEY']
 base_url = app.config['BASE_URL']
 
+def process_results(movie_list):
+    movie_results = list()
+    
+    for movie_item in movie_list:
+        id = movie_item.get("id")
+        title = movie_item.get("title")   
+        overview = movie_item.get("overview")    
+        poster = movie_item.get("poster_path")  
+        vote_average = movie_item.get("vote_average")    
+        vote_count = movie_item.get("vote_count")  
+        
+        if poster:
+            newMovieInstance = MovieObj(id,title,overview,poster,vote_average,vote_count)
+            movie_results.append(movieInstance)
+    return movie_results
+
 
 def get_movies(category):
     get_movies_url = base_url.format(category, api_key)
