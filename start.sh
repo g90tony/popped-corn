4c563eca8b563f6abb7faba4535cdeab8b54e0be
Caleb Tony <calebmbugua@gmail.com>
2021-04-19 07:58:44 +0300

    minor edits

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export API_KEY='70721ccaa728d675d5eaef4697fd2c57'
-export SECRET_KEY='<Flask WTF Secret Key>'
+# export API_KEY='70721ccaa728d675d5eaef4697fd2c57'
+# export SECRET_KEY='<Flask WTF Secret Key>'
 
 python manage.py server
