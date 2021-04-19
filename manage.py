e84e743953cdbb9b742a690dd30f3e53388882e1
Caleb Tony <calebmbugua@gmail.com>
2021-04-19 07:40:47 +0300

    commit message

--- manage.py
+++ manage.py
@@ -7,5 +7,5 @@ manager = Manager(app)
 manager.add_command('server',Server)
 
 manager.add_command('server',Server)
-if __name__ == '__main__':
-    manager.run()
+    if __name__ == '__main__':
+        manager.run()
