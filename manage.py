f1f1b46a0c77768c4ab7b8c319747931dbb4e89a
Caleb Tony <calebmbugua@gmail.com>
2021-04-21 15:07:38 +0300

    changed config env to development

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
 
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
