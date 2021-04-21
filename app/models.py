a5685088c65f5bd530d7d08aea2404d3623bfd71
Caleb Tony <calebmbugua@gmail.com>
2021-04-21 15:14:05 +0300

    added relation links between user and role

--- app/models.py
+++ app/models.py
@@ -44,6 +44,7 @@ class Role(db.model):
     
     id = db.Column(db.Integer, primary_key = true)
     name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy='dynamic')
     
     def __repr__(self):
         return f'User {self.name}'
@@ -51,6 +52,7 @@ class Role(db.model):
 class User (db.Model):
     __tablename__ = 'users'
     id= db.Column(db.String(255))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     
     def __repr__ (self):
         return f'User {self.username}'
