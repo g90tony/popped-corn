a6583c6ac5a0aeea77e5934868ea2f8c650b1d13
Caleb Tony <calebmbugua@gmail.com>
2021-04-21 12:34:14 +0300

    added db instantiator

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -14,6 +16,7 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
     
      # Registering the blueprint
     from .main import main as main_blueprint
@@ -22,7 +25,7 @@ def create_app(config_name):
     # setting config
     from .requests import configure_request
     configure_request(app)
-
+    
     # Will add the views and forms
 
     return app
