4833a9bf077ac0cd3eeda8d165e62e6fac263808
Caleb Tony <calebmbugua@gmail.com>
2021-04-21 14:57:28 +0300

    added manger shell

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
 
 app = create_app('production')
 
@@ -7,5 +8,11 @@ manager = Manager(app)
 manager.add_command('server',Server)
 
 manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User)
+
 if __name__ == '__main__':
     manager.run()
+    
